c2379fcf560a242d1a0b7ce024c765c400394683
abinkoshycheriyan <abincheriyan@gmail.com>
2019-10-26 09:03:46 +0530

    First Release
    Project Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  usersList: any[]=[]
+  name: any;
+  rates : [][]
 
-  constructor(public navCtrl: NavController) {
+  constructor(public http: HttpClient) {
+
+    this.http.get('https://lieservices.luluone.com:9443/liveccyrates?payload={%22activityType%22:%22rates.get%22,%22aglcid%22:784278,%22instype%22:%22LR%22}')
+
+    .subscribe(data=>{
+    //process the json data
+    console.log(data)
+    this.usersList=data["payload"]
+    console.log(this.usersList)
+    //alert(this.usersList.rates[0].ccyname)
+    this.name = this.usersList.rates[0].frmccy
+
+})
 
   }
 
